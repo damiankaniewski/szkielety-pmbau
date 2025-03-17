9af612d55e7cdc07b957aa471fa8ed523d0200bc
Damian <daba9@op.pl>
2025-03-17 18:04:59 +0100

    modules fix

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,2 +1,2 @@
-declare module 'three/examples/jsm/loaders/GLTFLoader';
-declare module 'three/examples/jsm/controls/OrbitControls';
+declare module 'three/examples/jsm/loaders/GLTFLoader.js';
+declare module 'three/examples/jsm/controls/OrbitControls.js';
